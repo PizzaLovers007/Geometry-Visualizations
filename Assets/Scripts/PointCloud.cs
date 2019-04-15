96cf88c3980987e7238652cbb39b669c6b82967e
Ditzley <sdh140330@utdallas.edu>
2019-04-14 22:07:35 -0500

    Added ConvexHull and PointCloud scripts and ConvexHull scene. ConvexHull script implements Graham Scan (upper/lower hull version) PointCloud generates a random (currently seeded for testing) cloud of vertices

---
+++ Assets/Scripts/PointCloud.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PointCloud : MonoBehaviour {
+    public int Size = 20;
+
+    public GameObject vertexPrefab;
+
+    public List<Vertex> Points { get; private set; }
+    
+    private void Awake() {
+        Points = new List<Vertex>();
+    }
+
+    private void Start() {
+        GenerateCloud();
+    }
+
+    private void Update() {
+        
+    }
+
+    private void GenerateCloud() {
+        Random.InitState(10);
+        float radius = 5f;
+
+        float[] densityZones = new float[] { 1f };
+
+        for (int i = 0; i < Size; i++) {
+            int zone = 0;
+            float f = Random.Range(0, 1);
+            float t = 0;
+            for (int d = 0; d < densityZones.Length; d++) {
+                t += densityZones[d];
+                if (f <= t) {
+                    zone = d;
+                    break;
+                }
+            }
+
+            float[] range = new float[] {
+                (((float)zone) / densityZones.Length) * radius,
+                (((float)zone + 1) / densityZones.Length) * radius,
+            };
+
+            float angle = Random.Range(0f, 360f);
+
+            Vector2 direction = Quaternion.AngleAxis(angle, Vector3.back) * Vector2.up;
+            Vector2 position = direction * Random.Range(range[0], range[1]);
+
+            Vertex vert = Instantiate(vertexPrefab, position, Quaternion.identity).GetComponent<Vertex>();
+            vert.transform.SetParent(transform);
+            Points.Add(vert);
+        }
+    }
+}
